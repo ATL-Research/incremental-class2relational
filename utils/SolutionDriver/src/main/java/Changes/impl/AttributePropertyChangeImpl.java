9b00dfbec347fe7e0ecac565085d11be94b544cf
Théo Le Calvar <tlc@kher.nl>
2023-05-01 16:32:57 +0200

    remove unused imports in AttributePropertyChangeImpl.java

--- utils/SolutionDriver/src/main/java/Changes/impl/AttributePropertyChangeImpl.java
+++ utils/SolutionDriver/src/main/java/Changes/impl/AttributePropertyChangeImpl.java
@@ -10,7 +10,6 @@ import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.ecore.EClass;
 
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
-import org.eclipse.emf.ecore.util.EcoreUtil;
 
 /**
  * <!-- begin-user-doc -->
