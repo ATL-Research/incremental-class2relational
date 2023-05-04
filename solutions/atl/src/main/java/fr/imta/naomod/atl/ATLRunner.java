41d4da4631e63092ff6722dd89386c2186e8dead
Théo Le Calvar <tlc@kher.nl>
2023-05-04 12:37:51 +0200

    switch ATL solution to use compiled metamodels

--- solutions/atl/src/main/java/fr/imta/naomod/atl/ATLRunner.java
+++ solutions/atl/src/main/java/fr/imta/naomod/atl/ATLRunner.java
@@ -4,14 +4,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EcorePackage;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
-import org.eclipse.m2m.atl.core.emf.EMFInjector;
-import org.eclipse.m2m.atl.core.emf.EMFModelFactory;
-import org.eclipse.m2m.atl.core.emf.EMFReferenceModel;
 import org.eclipse.m2m.atl.emftvm.EmftvmFactory;
 import org.eclipse.m2m.atl.emftvm.ExecEnv;
 import org.eclipse.m2m.atl.emftvm.Metamodel;
@@ -21,13 +13,11 @@ import org.eclipse.m2m.atl.emftvm.impl.resource.EMFTVMResourceFactoryImpl;
 import org.eclipse.m2m.atl.emftvm.util.DefaultModuleResolver;
 import org.eclipse.m2m.atl.emftvm.util.ModuleResolver;
 
-import Changes.ChangesPackage;
 import atl.research.AbstractDriver;
+import atl.research.class_.Class_Package;
+import atl.research.relational_.Relational_Package;
 
 public class ATLRunner extends AbstractDriver {
-
-	private Resource classMetamodel;
-	private Resource relationalMetamodel;
 	public static void main(String[] args) throws IOException {
 		try {
 			ATLRunner solution = new ATLRunner();
@@ -50,59 +40,14 @@ public class ATLRunner extends AbstractDriver {
 
 	@Override
 	protected void setupResourceSet() {
-		ResourceSet rs = getResourceSet();
+		super.setupResourceSet();
 
-		rs.getResourceFactoryRegistry().getExtensionToFactoryMap().put(
-            "xmi",
-            new XMIResourceFactoryImpl()
-        );
-
-		rs.getResourceFactoryRegistry().getExtensionToFactoryMap().put(
+		getResourceSet().getResourceFactoryRegistry().getExtensionToFactoryMap().put(
 			"emftvm",
 			new EMFTVMResourceFactoryImpl()
 		);
-		rs.getResourceFactoryRegistry().getExtensionToFactoryMap().put(
-			"ecore",
-			new XMIResourceFactoryImpl()
-		);
-		rs.getPackageRegistry().put(
-			EcorePackage.eNS_URI,
-			EcorePackage.eINSTANCE
-		);
-
-		// EMFTVM doesn't work with generated EPackage so we load metamodels from ecore file
-		classMetamodel = rs.getResource(URI.createURI("../../metamodels/Class.ecore"), true);
-		injectMetamodel(classMetamodel);
-
-		rs.getPackageRegistry().put(
-			"Class",
-			classMetamodel.getContents().get(0)
-		);
-
-		relationalMetamodel = rs.getResource(URI.createURI("../../metamodels/Relational.ecore"), true);
-		injectMetamodel(relationalMetamodel);
-
-		rs.getPackageRegistry().put(
-			"Relational",
-			relationalMetamodel.getContents().get(0)
-		);
-
-		rs.getPackageRegistry().put(
-			ChangesPackage.eNS_URI,
-			ChangesPackage.eINSTANCE
-		);
 	}
 
-
-	private void injectMetamodel(Resource resource) {
-		EMFModelFactory factory = new EMFModelFactory();
-		EMFInjector emfinjector = new EMFInjector();
-		EMFReferenceModel inMetamodel = (EMFReferenceModel) factory.newReferenceModel();
-		emfinjector.inject(inMetamodel, resource);
-		try {
-			emfinjector.inject(inMetamodel, (Resource)null);	// making sure garbage collecting the EMFReferenceModel will not unload the resource (see EMFModel.finalize())
-		} catch(NullPointerException e) {}
-	}
 
 	@Override
 	protected void applyTransformation() {
@@ -112,12 +57,12 @@ public class ATLRunner extends AbstractDriver {
 
 		// register source and target metamodels in ExecEnv
 		Metamodel sourceMM = EmftvmFactory.eINSTANCE.createMetamodel();
-		sourceMM.setResource(classMetamodel);
+		sourceMM.setResource(Class_Package.eINSTANCE.eResource());
 		execEnv.registerMetaModel("class_", sourceMM);
 
 
 		Metamodel targetMM = EmftvmFactory.eINSTANCE.createMetamodel();
-		targetMM.setResource(relationalMetamodel);
+		targetMM.setResource(Relational_Package.eINSTANCE.eResource());
 		execEnv.registerMetaModel("Relational", targetMM);
 		
 
