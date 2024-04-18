893ba0ab1d93cbeb43bbe018c2232be34bdbfecf
Nils Weidmann <nweidmann@itemis.com>
2024-04-18 15:34:12 +0200

    Add sources for the emoflon-neo solution

---
+++ solutions/emoflon-neo/org/eneo/class2relational/FirstToLowerCase.java
@@ -0,0 +1,58 @@
+// Setup 2
+package org.eneo.class2relational;
+
+//Setup 2
+import org.emoflon.neo.engine.modules.attributeConstraints.NeoAttributeConstraint;
+//Setup 2
+import org.emoflon.neo.engine.modules.attributeConstraints.NeoAttributeConstraintVariable;
+
+//Setup 4 (Nils 5)
+public class FirstToLowerCase extends NeoAttributeConstraint {
+
+	/**
+	 * FirstToLowerCase(word, lowerCase) - lowerCase is word with its first character in lower case.
+	 * 
+	 */
+	@Override
+	// Setup 2 (Nils 3)
+	public void solve() {
+		// Setup 5
+		if (variables.size() != 2)
+			// Setup 4
+			throw new RuntimeException("The CSP -ToLowerCase- needs exactly two variables");
+
+		// Setup 3 (Nils 5)
+		NeoAttributeConstraintVariable word = variables.get(0);
+		// Setup 3 (Nils 5)
+		NeoAttributeConstraintVariable lowerCase = variables.get(1);
+
+		// Setup 5
+		String bindingStates = getBindingStates(word, lowerCase);
+
+		// BB - simple check
+		// Tracing 4
+		if (bindingStates.equals("BB")) {
+			// Tracing 9
+			setSatisfied(firstToLower(word.getValue().toString()).equals(lowerCase.getValue().toString()));
+		}
+		// BF - make first letter lower case
+		// Transformation 5
+		else if (bindingStates.equals("BF")) {
+			// Transformation 6
+			lowerCase.bindToValue(firstToLower(word.getValue().toString()));
+			// Setup 2
+			setSatisfied(true);
+		// Setup 1 (Nils)
+		} else {
+			// Setup 5 (Nils 6)
+			throw new UnsupportedOperationException(
+					"This case in the constraint has not been implemented yet: " + bindingStates);
+		}
+	}
+
+	// Setup 5
+	private String firstToLower(String s) {
+		// Transformation 6 (Nils 11)
+		return s.toLowerCase().charAt(0) + s.substring(1, s.length());
+	}
+}
