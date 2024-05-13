c9fe0d4b99a21396599569e694356f4935f25f4b
Théo Le Calvar <theo.le-calvar@imt-atlantique.fr>
2024-05-13 19:48:50 +0200

    make Makefile and Rscript generic

--- metrics/csharp/csharp-extract-metric-data.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-echo "Transformation Aspect, Value" > csharp-metrics.csv
-grep -E "// Setup| // Model Traversal| // Helper| // Expression Outsourcing| // Tracing| // Incrementality| // Transformation| // Model Navigation| // Change Propagation" ../../solutions/csharp/CSharpClassToRelational.cs | sed -e 's/^[[:space:]]*\/\/\?//' | sed -e 's/\([0-9]\)/, \1/' | sed 's/Expression Outsourcing/Helper/g' >> csharp-metrics.csv
