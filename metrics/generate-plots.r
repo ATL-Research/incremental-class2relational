de0af59439b274e3808771b748cbdb5f5b9b8c8f
Sandra Greiner <greinersandrauni@gmail.com>
2025-04-16 14:10:37 +0200

    update to have a fixed colore code for each transformation aspect

--- metrics/generate-plots.r
+++ metrics/generate-plots.r
@@ -10,33 +10,33 @@ savePlot <- function(plot, name) {
 createPlot <- function(data, title) {
 
 	# Define the order of types for stacking
-	type_order <- c("SETUP", "MODEL_TRAVERSAL", "CHANGE_PROPAGATION", "HELPER", "TRACING", "CHANGE_IDENTIFICATION", "TRANSFORMATION", "EMPTY", "WRAPPER")
+	type_order <- c("SETUP", "MODEL_TRAVERSAL", "CHANGE_PROPAGATION", "HELPER", "TRACING", "CHANGE_IDENTIFICATION", "TRANSFORMATION", "EMPTY")
+    color_values = c("#F8766D", "#C49A00", "#53B400", "#00C094", "#00B5EB", "#619CFF", "#ABABAB", "#FF00FF", "#EFEFEF")
 
+	names(color_values) <- type_order
 	# Fill missing types with 0 values
-	missing_types <- setdiff(type_order, unique(data$Transformation.Aspect))
-	missing_data <- data.frame(File.name=0,Transformation.Aspect = missing_types, Value = 0, Lines=0)
-	data <- rbind(data, missing_data)
+	#missing_types <- setdiff(type_order, unique(data$Transformation.Aspect))
+	#missing_data <- if (!length(missing_types)) data.frame(File.name=0,Transformation.Aspect = missing_types, Value = 0, Lines=0) else NULL
+	#data <- rbind(data, missing_data)
 	
-	# Calculate the total value for each type
-	data_summary <- data %>%
-	  group_by(Transformation.Aspect) %>%
-	  summarise(Value = sum(Value))
+    data <- data %>% mutate(Transformation.Aspect = factor(Transformation.Aspect, levels = type_order))
 
-	# Calculate the total and percentage values
-	data_summary <- data_summary %>%
-		  mutate(Total = sum(Value),
-	         Percentage = Value / Total * 100)
+	# Calculate the total value for each typetype_ord
+	data_summary <- data %>%
+	  mutate(Total = sum(Value)) %>%
+	  group_by(Transformation.Aspect) %>%
+	  reframe(Value = sum(Value), Percentage = Value / Total * 100) %>% unique()
 
 	# Create the stacked bar plot
 	plot <- ggplot(data_summary, aes(x = "", y = Value, fill = Transformation.Aspect)) +
 	  geom_bar(stat = "identity") +
-  	  scale_fill_manual(values = c("#F8766D", "#C49A00", "#53B400", "#00C094", "#00B5EB", "#619CFF", "#ABABAB", "#FF00FF", "#EFEFEF")) +
+  	  scale_fill_manual(values = color_values) +
 	  xlab("Transformation Aspect") +
 	  ylab("Word Count") +
 	  ggtitle(title) +
 	  theme_bw() +
 	  theme(plot.title = element_text(hjust = 0.5)) +
-	  guides(fill = guide_legend(reverse = TRUE, override.aes = list(order = type_order))) +
+	  #guides(fill = guide_legend(reverse = TRUE, override.aes = list(order = type_order))) #+
 	  geom_text(aes(label = paste0(Value, " (", round(Percentage, 1), "%)")), 
             position = position_stack(vjust = 0.5), size = 3)
 }
